66c78b85800d3beb296aa901af60b6aa73c85afa
Fahim Foysal Abak <abakfahim@gmail.com>
2024-05-13 02:01:24 +0600

    first-commit

---
+++ app/medicineList/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+export default function page() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <h1>Medicine List</h1>
+    </>
+  )
+}
