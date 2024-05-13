a258b33d978f31de516f9b743efbe4b97b7c0104
Fahim Foysal Abak <abakfahim@gmail.com>
2024-05-13 13:45:54 +0600

    card-design

--- app/ui/medicinelist/Table.js
+++ app/ui/medicinelist/Table.js
@@ -2,106 +2,145 @@
 // import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 // import InvoiceStatus from '@/app/ui/invoices/status';
 // import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
- import { fetchFilteredMedicine } from '@/app/lib/data';
+import { fetchFilteredMedicine } from '@/app/lib/data';
 
 export default async function MedicineTable({
-  query,
-  currentPage,
+    query,
+    currentPage,
 }) {
-  const medicines = await fetchFilteredMedicine(query, currentPage);
+    const medicines = await fetchFilteredMedicine(query, currentPage);
 
-  return (
-    <div className="mt-6 flow-root">
-      <div className="inline-block min-w-full align-middle">
-        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <div className="md:hidden">
-            {medicines?.map((medicine) => (
-              <div
-                key={medicine.id}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
-                <div className="flex items-center justify-between border-b pb-4">
-                  <div>
-                    <div className="mb-2 flex items-center">
-                      {/* <Image
-                        src={invoice.image_url}
-                        className="mr-2 rounded-full"
-                        width={28}
-                        height={28}
-                        alt={`${invoice.name}'s profile picture`}
-                      /> */}
-                      <p className='text-xl font-medium'>{medicine.brandname}</p>
+    return (
+        <div className="mt-6 flow-root">
+            <div className="inline-block min-w-full align-middle">
+                <div className="rounded-lg bg-gray-50 p-2 md:pt-2">
+                    {/* <div className="md:hidden">
+                        {medicines?.map((medicine) => (
+                            <div
+                                key={medicine.id}
+                                className="mb-2 w-full rounded-md bg-white p-4"
+                            >
+                                <div className="flex items-center justify-between border-b pb-4">
+                                    <div>
+                                        <div className="mb-2 flex items-center">
+                                            <Image
+                                                src={invoice.image_url}
+                                                className="mr-2 rounded-full"
+                                                width={28}
+                                                height={28}
+                                                alt={`${invoice.name}'s profile picture`}
+                                            />
+                                            <p className='text-xl font-medium'>{medicine.brandname}</p>
+                                        </div>
+                                        <p className="text-sm text-gray-500">{medicine.genericname}</p>
+                                    </div>
+                                    <InvoiceStatus status={invoice.status} />
+                                </div>
+                                <div className="flex w-full items-center justify-between pt-4">
+                                    <div>
+                                        <p className="text-green-500">
+                                            {medicine.nameofthemanufacturer}
+                                        </p>
+                                        <p className='font-medium'>{medicine.price}</p>
+                                    </div>
+                                    <div className="flex justify-end gap-2">
+                                        <UpdateInvoice id={invoice.id} />
+                                        <DeleteInvoice id={invoice.id} />
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div> */}
+
+                    <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                        {medicines?.map((medicine) => (
+                            <div
+                                key={medicine.id}
+                                className="rounded-md bg-white p-4 shadow-lg"
+                            >
+                                <div className="flex items-center justify-between border-b pb-4">
+                                    <div>
+                                        <div className="mb-2 flex items-center">
+                                            {/* <Image
+              src={invoice.image_url}
+              className="mr-2 rounded-full"
+              width={28}
+              height={28}
+              alt={`${invoice.name}'s profile picture`}
+            /> */}
+                                            <p className='text-xl font-medium'>{medicine.brandname}</p>
+                                        </div>
+                                        <p className="text-sm text-gray-500">{medicine.genericname}</p>
+                                    </div>
+                                    {/* <InvoiceStatus status={invoice.status} /> */}
+                                </div>
+                                <div className="flex items-center justify-between pt-4">
+                                    <div>
+                                        <p className="text-green-500">
+                                            {medicine.nameofthemanufacturer}
+                                        </p>
+                                        <p className='font-medium'>{medicine.price}</p>
+                                    </div>
+                                    {/* <div className="flex justify-end gap-2">
+          <UpdateInvoice id={invoice.id} />
+          <DeleteInvoice id={invoice.id} />
+        </div> */}
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    <p className="text-sm text-gray-500">{medicine.genericname}</p>
-                  </div>
-                  {/* <InvoiceStatus status={invoice.status} /> */}
-                </div>
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-green-500">
-                      {medicine.nameofthemanufacturer}
-                    </p>
-                    <p className='font-medium'>{medicine.price}</p>
-                  </div>
-                  {/* <div className="flex justify-end gap-2">
-                    <UpdateInvoice id={invoice.id} />
-                    <DeleteInvoice id={invoice.id} />
-                  </div> */}
-                </div>
-              </div>
-            ))}
-          </div>
-          <table className="hidden min-w-full text-gray-900 md:table">
-            <thead className="rounded-lg text-left text-sm font-normal">
-              <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Brand Name
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Generic Name
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Manufacturer
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Price
-                </th>
-                {/* <th scope="col" className="px-3 py-5 font-medium">
+
+                    {/* <table className="hidden min-w-full text-gray-900 md:table">
+                        <thead className="rounded-lg text-left text-sm font-normal">
+                            <tr>
+                                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                                    Brand Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Generic Name
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Manufacturer
+                                </th>
+                                <th scope="col" className="px-3 py-5 font-medium">
+                                    Price
+                                </th> */}
+                    {/* <th scope="col" className="px-3 py-5 font-medium">
                   Status
                 </th>
                 <th scope="col" className="relative py-3 pl-6 pr-3">
                   <span className="sr-only">Edit</span>
                 </th> */}
-              </tr>
-            </thead>
-            <tbody className="bg-white">
-              {medicines?.map((medicine) => (
-                <tr
-                  key={medicine.id}
-                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
-                >
-                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                    <div className="flex items-center gap-3">
-                      {/* <Image
+                    {/* </tr>
+                        </thead>
+                        <tbody className="bg-white">
+                            {medicines?.map((medicine) => (
+                                <tr
+                                    key={medicine.id}
+                                    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                                >
+                                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                                        <div className="flex items-center gap-3"> */}
+                    {/* <Image
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
                         height={28}
                         alt={`${invoice.name}'s profile picture`}
                       /> */}
-                      <p className='font-semibold'>{medicine.brandname}</p>
-                    </div>
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3">
-                    {medicine.genericname}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3 text-green-500">
-                    {medicine.nameofthemanufacturer}
-                  </td>
-                  <td className="whitespace-nowrap px-3 py-3 font-semibold">
-                    {medicine.price}
-                  </td>
-                  {/* <td className="whitespace-nowrap px-3 py-3">
+                    {/* <p className='font-semibold'>{medicine.brandname}</p>
+                                        </div>
+                                    </td>
+                                    <td className="whitespace-nowrap px-3 py-3">
+                                        {medicine.genericname}
+                                    </td>
+                                    <td className="whitespace-nowrap px-3 py-3 text-green-500">
+                                        {medicine.nameofthemanufacturer}
+                                    </td>
+                                    <td className="whitespace-nowrap px-3 py-3 font-semibold">
+                                        {medicine.price}
+                                    </td> */}
+                    {/* <td className="whitespace-nowrap px-3 py-3">
                     <InvoiceStatus status={invoice.status} />
                   </td>
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
@@ -110,12 +149,12 @@ export default async function MedicineTable({
                       <DeleteInvoice id={invoice.id} />
                     </div>
                   </td> */}
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                    {/* </tr>
+                            ))}
+                        </tbody>
+                    </table> */}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
