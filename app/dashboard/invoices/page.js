f918f1518b8609f47ade3e82dbd8ad0f9cf2cfab
Fahim Foysal Abak <abakfahim@gmail.com>
2024-05-16 02:10:09 +0600

    fixed suspense

--- app/dashboard/invoices/page.js
+++ app/dashboard/invoices/page.js
@@ -10,12 +10,12 @@ export default function page() {
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
             </div>
-            {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
                 <Suspense>
                     <Search placeholder="Search invoices..." />
                 </Suspense>
                 <CreateInvoice />
-            </div> */}
+            </div>
 
             <div className="mt-5 flex w-full justify-center">
                 {/* <Pagination totalPages={totalPages} /> */}
