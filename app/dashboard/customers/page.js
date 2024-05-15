ab0233c16b705a9fbe37d5e56109c08a6482edca
Fahim Foysal Abak <abakfahim@gmail.com>
2024-05-16 02:01:30 +0600

    Customer add

--- app/dashboard/customers/page.js
+++ app/dashboard/customers/page.js
@@ -9,29 +9,31 @@ import CustomersTable from '@/app/ui/customers/table'
 import Pagination from '@/app/ui/customers/pagination'
 
 
-export default async function page({searchParams}) {
+export default async function page({ searchParams }) {
 
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
   const totalPages = await fetchCustomerPages(query);
   return (
     <>
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search customers..." />
-                <CreateCustomer />
-            </div>
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Suspense>
+          <Search placeholder="Search customers..." />
+        </Suspense>
+        <CreateCustomer />
+      </div>
 
-            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <CustomersTable query={query} currentPage={currentPage} />
       </Suspense>
 
-            <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
-            </div>
-        
-        </>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+
+    </>
   )
 }
--- app/dashboard/invoices/page.js
+++ app/dashboard/invoices/page.js
@@ -1,6 +1,6 @@
 import { CreateInvoice } from '@/app/ui/invoices/button'
 import Search from '@/app/ui/search'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { lusitana } from '@/app/ui/fonts'
 
 export default function page() {
@@ -11,14 +11,16 @@ export default function page() {
                 <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search invoices..." />
+                <Suspense>
+                    <Search placeholder="Search invoices..." />
+                </Suspense>
                 <CreateInvoice />
             </div>
 
             <div className="mt-5 flex w-full justify-center">
                 {/* <Pagination totalPages={totalPages} /> */}
             </div>
-        
+
         </>
     )
 }
